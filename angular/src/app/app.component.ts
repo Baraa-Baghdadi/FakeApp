e98a83cf10155cf01540c4ac5b0a3b10cffd26d5
Baraa-Baghdadi <barrasy19999@gmail.com>
2024-09-06 16:44:17 +0100

    Make new design for header

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { AuthService } from '@abp/ng.core';
+import { AuthService, ReplaceableComponentsService } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
 import { SignalRService } from './services/signalR/signal-r.service';
+import { eThemeSharedComponents } from '@abp/ng.core';
+import { eThemeLeptonXComponents } from '@abp/ng.theme.lepton-x';
+import { NewHeaderComponent } from './welcome/new-header/new-header.component';
+
 
 @Component({
   selector: 'app-root',
@@ -12,12 +16,18 @@ import { SignalRService } from './services/signalR/signal-r.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private authService:AuthService,private signalR:SignalRService){
+  constructor(private authService:AuthService,private signalR:SignalRService,
+    private replaceableComponent: ReplaceableComponentsService,
+  ){
   }
 
   ngOnInit(): void {
     if (this.authService.isAuthenticated){
       this.signalR.connect();
     }
+    this.replaceableComponent.add({
+      component: NewHeaderComponent,
+      key: eThemeLeptonXComponents.Languages,
+    });
   }
 }
